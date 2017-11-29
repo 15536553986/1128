6b962bc7c42e21723697d220fc30b1312335dcac
“your_u <your_registere15536553986@163.com>
2017-11-29 09:49:28 +0800

    11月28日

---
+++ one.php
@@ -0,0 +1,72 @@
+<html>
+  	<?php
+		echo "这是php第一个代码";
+		echo "这是php第一个代码";
+	?>
+	<head >
+	<?php
+		
+		echo "这是php第二个代码"
+	
+	?>
+		<title >
+			这是一个网站
+		</title>
+		
+		<meta charset="utf-8" />
+		
+		<style 	>
+			
+		div{
+		color: red;	
+		}
+		
+		
+			
+		
+		
+		
+		
+		</style>
+		
+	</head>
+	
+	<body>
+
+	<div style=" color: <?php echo "red" ?> ;  ">
+		这是一个php程序
+		
+	</div>
+	    	
+	  	<?php
+		
+		echo "这是php第sange个代码"
+		
+		?>
+	  <script>
+	
+	  	
+	  	<?php
+	     echo "for (var i = 0; i < 10; i++) {"
+		?>
+			
+				 document.write("js代码" +i );
+		
+		<?php
+		
+	    echo "}"
+		
+		?>
+	  
+	  	
+	
+	  </script>
+		
+		
+	</body>
+	
+</html>
+	<?php
+		
+	    
+		
